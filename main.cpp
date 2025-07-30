fc5db85d95f6698db1ca4e27525aa1e35b2e1d40
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-30 13:38:33 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <malloc.h>
+
+using namespace std;
+
+struct Node {
+    int value;
+    Node* next;
+};
+
+bool isEmpty(Node* front, Node *rear) {
+    return front == rear;
+}
+
+void push() {
+
+}
+
+void pop() {
+
+}
+
+void front() {
+
+}
+
+void rear() {
+
+}
+
+void menu() {
+
+}
+
+void runProgram() {
+    Node *front;
+    Node *rear;
+
+    front = rear = NULL;
+
+
+}
+
+int main() {
+
+}

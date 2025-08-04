1bd7f545a3fc1c2da2baf656bdf418804d696d67
nina <juan.amado.sicat.cleto@gmail.com>
2025-08-04 21:50:11 +0800

    Orayt!

--- main.cpp
+++ main.cpp
@@ -9,27 +9,69 @@ struct Node {
 };
 
 bool isEmpty(Node* front, Node *rear) {
-    return front == rear;
+    return front == NULL && rear == NULL;
 }
 
-void push() {
+void push(Node **front, Node **rear, int item) {
+    Node* ptr;
 
+    ptr = (Node*)malloc(sizeof(Node));
+    ptr->value = item;
+    ptr->next = NULL;
+
+    if (isEmpty(*front, *rear)) {
+        *front = ptr;
+        *rear = ptr;
+    }
+    else {
+        (*rear)->next = ptr;
+        *rear = ptr;
+    }
+    cout << "Item has been added!" << endl;
 }
 
-void pop() {
+int pop(Node **front, Node **rear) {
+    Node *ptr;
 
+    int item;
+
+    if (isEmpty(*front, *rear)) {
+        cout << "Queue is empty!" << endl;
+    }
+    else {
+        item = (*front)->value;
+        ptr = *front;
+        *front = (*front)->next;
+    }
+
+    if  (*front == NULL) {
+        *rear = NULL;
+    }
+    free(ptr);
+
+    cout << "Item has been removed!" << endl;
+    return item;
 }
 
-void front() {
-
-}
-
-void rear() {
-
+int front(Node *front, Node *rear) {
+    if (isEmpty(front, rear)) {
+        cout << "Queue is empty!" << endl;
+    }
+    else {
+        return front->value;
+    }
 }
 
 void menu() {
-
+    cout << "=====================================================" << endl;
+    cout << "                      Le Queue                       " << endl;
+    cout << "-----------------------------------------------------" << endl;
+    cout << "1. Push" << endl;
+    cout << "2. Pop" << endl;
+    cout << "3. Front" << endl;
+    cout << "4. Exit" << endl;
+    cout << "=====================================================" << endl;
+    cout << "Choice: ";
 }
 
 void runProgram() {
